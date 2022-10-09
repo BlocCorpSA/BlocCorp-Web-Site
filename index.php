fca453a58dd46b93c5c1c9f111b687b46f3d63cd
BlocBlocTheBloc <97693441+BlocCorp@users.noreply.github.com>
2022-10-09 12:21:16 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>BlocCorp Web Site ENGLISH</title>
+	<style type="text/css"></style>
+</head>
+<body>
+	<h1 style="color: darkcyan;">Welcome to BlocCorp's Web site !</h1>
+
+</body>
+</html>
